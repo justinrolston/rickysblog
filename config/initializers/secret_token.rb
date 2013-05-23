87c8edab3e4783e7fb9246dcaa751e1557a6cef0
Ricky Rickard, Jr <rerickardjr@gmail.com>
2013-05-23 19:46:28 -0400

    initcommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rickysblog::Application.config.secret_token = 'ebb31f369170251e13d6a7ed3b10674107edec959572c4eea435d9c96bc4ed66908c3976d6825818c1089551f1f4c7f65dfe619221ad40af3ba7f4b70b3d1654'
