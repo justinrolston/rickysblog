26c26729b5b2806d6d707719e8036f9e8c717f73
Ricky Rickard, Jr <rerickardjr@gmail.com>
2013-05-27 18:28:45 -0400

    issues11

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,6 @@ class Post < ActiveRecord::Base
   has_many :comments, :dependent => :destroy
   has_many :tags
 
-  accepts_nested_attributes_for :tags, :allow_destroy => :true
-    :reject_if =>proc { |attrs| attrs.all? { |k, v| v.blank? } }
+  accepts_nested_attributes_for :tags, :allow_destroy => :true,
+    :reject_if = proc { |attrs| attrs.all? { |k, v| v.blank? } }
 end
